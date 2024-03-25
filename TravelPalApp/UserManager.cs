1f090e6f8b4f17c8beb8d5e9ec6fb62daee1a80d
AminElharrab <aminelharrab8@gmail.com>
2024-03-25 17:26:08 +0100

    UserManager

--- TravelPalApp/UserManager.cs
+++ TravelPalApp/UserManager.cs
@@ -11,14 +11,55 @@ namespace TravelPalApp
 {
     public static class UserManager
     {
+        public static List<IUser> users = new List<IUser>();
+        public static IUser SignedInUser { get; private set; }
+
+        public static bool AddUser(IUser user)
+        {
+            if (user == null || string.IsNullOrEmpty(user.Username) || string.IsNullOrEmpty(user.Password)) 
+                return false;
+
+
+            if (!ValidateUsername(user.Username))
+                return false;
+                
+                 users.Add(user);
+                 return true;
+        }
+        public static bool UpdateUsername(IUser user, string NewUsername)
+        {
+           if (user == null || string.IsNullOrEmpty(NewUsername))
+              return false;
+
+            if (!ValidateUsername(user.Username))
+                return false;
+
+            user.Username = NewUsername;
+            return true;
+        }
+        private static bool ValidateUsername(string username)
+            {
+                return !string.IsNullOrEmpty(username);
+            }
+        public static void RemoveUser(IUser user)
+        {
+            if (user == null)
+                return;
+
+            users.Remove(user);
+        }
+        public static bool SignInUser(string username, string password)
+        {
+
+            foreach (IUser user in users)
+            {
+                if (user.Username == username && user.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
     }
-
-
-
-
-
-
-
-           
-
+}
