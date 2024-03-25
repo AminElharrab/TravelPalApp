63fafbf5cb0a7a4831b7da84387ef3f346680031
AminElharrab <aminelharrab8@gmail.com>
2024-03-25 22:04:17 +0100

    abc

--- TravelPalApp/RegisterWindow.xaml.cs
+++ TravelPalApp/RegisterWindow.xaml.cs
@@ -14,9 +14,6 @@ using System.Windows.Shapes;
 
 namespace TravelPalApp
 {
-    /// <summary>
-    /// Interaction logic for RegisterWindow.xaml
-    /// </summary>
     public partial class RegisterWindow : Window
     {
         public RegisterWindow()
